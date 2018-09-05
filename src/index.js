8f73ea54fb1ef8a2441b97ce10e35a53467db2a1
Matthew Langford <Matthew.Langford@softwire.com>
2018-09-05 17:46:41 +0100

    Master: Working fizzbuzz program.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 function FizzBuzz() {
-    return 'Hello world';
+    return FizzBuzzTo100();
+}
+function FizzBuzzTo100() {
+    var str = "";
+    for (var i = 1; i <= 100; i++) {
+        str = str.concat(constructLine(i) + "<br/>");
+    }
+    return str;
+}
+function constructLine(i) {
+    if (!(Fizz(i) || Buzz(i))) {
+        return i.toString();
+    }
+    var line = "";
+    if (Fizz(i)) {
+        line = line.concat("Fizz");
+    }
+    if (Buzz(i)) {
+        line = line.concat("Buzz");
+    }
+    return line;
+}
+function Fizz(i) {
+    return i % 3 === 0;
+}
+function Buzz(i) {
+    return i % 5 === 0;
 }
 document.body.innerHTML = FizzBuzz();
