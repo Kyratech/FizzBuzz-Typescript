9bab285184d9ed3048df80c759d2aa97446ee317
Matthew Langford <Matthew.Langford@softwire.com>
2018-09-06 10:44:56 +0100

    Master: Refactored into multiple files and removed .js from git.

---
+++ scripts/FizzBussRules.ts
@@ -0,0 +1,48 @@
+export interface FizzBuzzRule {
+    applyRuleIfNeeded(n: number, currentOutput: Array<string>): Array<string>;
+}
+
+abstract class MasterFizzBuzzRule implements FizzBuzzRule {
+    public applyRuleIfNeeded(n: number, currentOutput: Array<string>): Array<string> {
+        if (this.numberIsRule(n)) {
+            return this.executeRule(this.removeNumberFromOutput(n, currentOutput));
+        }
+        return currentOutput;
+    }
+
+    abstract numberIsRule(n: number): boolean;
+    abstract executeRule(output: Array<string>): Array<string>;
+
+    //If no previous rules are applied, the output should be a single number
+    private removeNumberFromOutput(n: number, currentOutput: Array<string>): Array<string> {
+        if (currentOutput.length === 1) {
+            if (!isNaN(Number(currentOutput[0])))
+            {
+                return [];
+            }
+        }
+        return currentOutput;
+    }
+}
+
+export class FizzRule extends MasterFizzBuzzRule {
+    numberIsRule(n: number): boolean {
+        return n % 3 === 0;
+    }
+
+    executeRule(output: Array<string>): Array<string> {
+        output.push("Fizz");
+        return output;
+    }
+}
+
+export class BuzzRule extends MasterFizzBuzzRule {
+    numberIsRule(n: number): boolean {
+        return n % 5 === 0;
+    }
+
+    executeRule(output: Array<string>): Array<string> {
+        output.push("Buzz");
+        return output;
+    }
+}
---
+++ scripts/index.ts
@@ -0,0 +1,29 @@
+import {FizzBuzzRule, FizzRule, BuzzRule} from './FizzBussRules';
+
+const Rules: Array<FizzBuzzRule> = [new FizzRule(), new BuzzRule()];
+
+function FizzBuzz(): string {
+    return FizzBuzzTo100();
+}
+
+function FizzBuzzTo100(): string {
+    let str = "";
+    
+    for(let i = 1; i <= 100; i++) {
+        str = str.concat(constructLine(i) + "<br/>");
+    }
+    
+    return str;
+}
+
+function constructLine(n: number): string {
+    let line: Array<string> = [n.toString()];
+
+    for(let j = 0; j < Rules.length; j++) {
+        line = Rules[j].applyRuleIfNeeded(n, line);
+    }
+
+    return line.join("");
+}
+
+document.body.innerHTML = FizzBuzz();
