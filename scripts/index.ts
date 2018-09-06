833905370f71a796212603e1dda1f0d1c2d8d66b
Matthew Langford <Matthew.Langford@softwire.com>
2018-09-06 12:26:51 +0100

    Master: Added form to webpage for user to select no. of results.

--- scripts/index.ts
+++ scripts/index.ts
@@ -7,14 +7,19 @@ const Rules: Array<FizzBuzzRule> = [new FizzRule(),
     new FezzRule(),
     new ReverseRule()];
 
-function FizzBuzz(): string {
-    return FizzBuzzTo300();
+function RunFizzBuzz(): void {
+    (document.getElementById('fizzbuzz-content')).innerHTML = FizzBuzz();
 }
 
-function FizzBuzzTo300(): string {
+function FizzBuzz(): string {
+    let n: number = parseInt((<HTMLInputElement>document.getElementById('no-results')).value);
+    return FizzBuzzToN(n);
+}
+
+function FizzBuzzToN(n: number): string {
     let str = "";
     
-    for(let i = 1; i <= 300; i++) {
+    for(let i = 1; i <= n; i++) {
         str = str.concat(constructLine(i) + "<br/>");
     }
     
@@ -31,4 +36,4 @@ function constructLine(n: number): string {
     return line.join("");
 }
 
-document.body.innerHTML = FizzBuzz();
+document.getElementById("fizzbuzz-form").addEventListener("submit", RunFizzBuzz, true);
