333d8f92822901f2b49d873bd605b784e7f39660
Matthew Langford <Matthew.Langford@softwire.com>
2018-09-06 12:55:49 +0100

    Master: Added controls to choose which rules are applied.

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,6 +1,6 @@
 import {FizzBuzzRule, FizzRule, BuzzRule, BangRule, BongRule, FezzRule, ReverseRule} from './FizzBuzzRules';
 
-const Rules: Array<FizzBuzzRule> = [new FizzRule(),
+let Rules: Array<FizzBuzzRule> = [new FizzRule(),
     new BuzzRule(),
     new BangRule(),
     new BongRule(),
@@ -8,12 +8,45 @@ const Rules: Array<FizzBuzzRule> = [new FizzRule(),
     new ReverseRule()];
 
 function RunFizzBuzz(): void {
+    SetRules();
     (document.getElementById('fizzbuzz-content')).innerHTML = FizzBuzz();
+}
+
+function SetRules(): void {
+    Rules = [];
+    
+    if (IsChecked('fizz')) {
+        Rules.push(new FizzRule());
+    }
+
+    if (IsChecked('buzz')) {
+        Rules.push(new BuzzRule());
+    }
+
+    if (IsChecked('bang')) {
+        Rules.push(new BangRule());
+    }
+
+    if (IsChecked('bong')) {
+        Rules.push(new BongRule());
+    }
+
+    if (IsChecked('fezz')) {
+        Rules.push(new FezzRule());
+    }
+
+    if (IsChecked('reverse')) {
+        Rules.push(new ReverseRule());
+    }
 }
 
 function FizzBuzz(): string {
     let n: number = parseInt((<HTMLInputElement>document.getElementById('no-results')).value);
     return FizzBuzzToN(n);
+}
+
+function IsChecked(elementId: string): boolean {
+    return (<HTMLInputElement>document.getElementById(elementId)).checked;
 }
 
 function FizzBuzzToN(n: number): string {
